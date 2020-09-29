02ca8a4423c3d98a40e5e1922a61f2122fec423a
Vinicius Mazon <vnpmazon@gmail.com>
2020-09-28 22:38:37 -0300

    feat: Adds Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+
+      {/* <Footer /> */}
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Repo: React.FC = () => {
+  return (
+    <h1>Repo</h1>
+  );
+}
+
+export default Repo;
